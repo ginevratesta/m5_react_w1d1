48b6bee08b7326373eaddda6a5c529f531d129e5
Ginevra Testa <ginevra99testaa@gmail.com>
2024-02-07 13:59:10 +0100

    last commit

--- src/components/displayQuiz.js
+++ src/components/displayQuiz.js
@@ -1,6 +1,6 @@
 import React from "react";
 import FormatHTMLQuiz from "./formatHTMLQuiz";
-import quizArray from "./q&a";
+import quizArray from "./quizArray";
 
 const DisplayQuiz = () => {
   return quizArray.map((quiz) => <FormatHTMLQuiz quiz={quiz} />);
